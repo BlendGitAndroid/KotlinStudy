eda245ddd10d6a84dbe3a181646131b285b2bcf3
BlendAndroid <1059152860@qq.com>
2021-12-05 18:20:33 +0800

    feat：kotlin version

--- coroutines/src/main/java/com/blend/coroutines/CoroutinesTest.kt
+++ coroutines/src/main/java/com/blend/coroutines/CoroutinesTest.kt
@@ -1,7 +1,16 @@
 package com.blend.coroutines
 
-class CoroutinesTest {
-    fun test() {
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+fun main(args: Array<String>) {
+
+    test()
+}
+
+fun test() {
+    GlobalScope.launch {
 
     }
 }
